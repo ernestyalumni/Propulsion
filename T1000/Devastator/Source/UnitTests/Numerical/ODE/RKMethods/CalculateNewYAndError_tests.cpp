44bafe5facfd3957aa5c91ac00334a36776b0350
ernestyalumni <ernestyalumni@gmail.com>
2022-04-14 01:05:14 -0700

    Fixed unit tests for RK4 and Dormand-Prince 5

--- T1000/Devastator/Source/UnitTests/Numerical/ODE/RKMethods/CalculateNewYAndError_tests.cpp
+++ T1000/Devastator/Source/UnitTests/Numerical/ODE/RKMethods/CalculateNewYAndError_tests.cpp
@@ -78,15 +78,28 @@ class Examplef
       const vector<double>& y,
       vector<double>& output)
     {
-      transform(
-        y.begin(),
-        y.end(),
-        back_inserter(output),
-        [x](const double y_value)
-        {
-          return y_value - x * x + 1.0;
-        }
-      ); 
+      if (output.size() == 0)
+      {
+        transform(
+          y.begin(),
+          y.end(),
+          back_inserter(output),
+          [x](const double y_value)
+          {
+            return y_value - x * x + 1.0;
+          });
+      }
+      else
+      {
+        transform(
+          y.begin(),
+          y.end(),
+          output.begin(),
+          [x](const double y_value)
+          {
+            return y_value - x * x + 1.0;
+          });        
+      }
     }
 
     vector<double> operator()(
@@ -394,18 +407,238 @@ TEST(TestCalculateNewYAndError, SumAAndKProductsWorksForRK4Method)
   EXPECT_DOUBLE_EQ(setup.y_out_.at(0), 0.453125);
 }
 
-/*
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
-TEST(TestCalculateNewYAndError, CalculateNewYCalculatesKCoefficients)
+TEST(TestCalculateNewYAndError, StepsForCalculateNewYOnRK4Coefficients)
 {
   ExampleSetup setup {};
+  EXPECT_EQ(setup.y_out_.size(), 1);
+  EXPECT_EQ(setup.y_out_.at(0), 0.0);
+
+  Examplef f {};
+  // For RK4 method.
+  KCoefficients<RK4_s, vector<double>> k_coefficients;
+  k_coefficients.ith_coefficient(1) = setup.dydx_0_;
+
+  TestCalculateNewYAndError<RK4_s, decltype(f)> new_y_and_err {
+    f,
+    RK4_a_coefficients,
+    RK4_c_coefficients,
+    RK4_b_coefficients};
+
+  const double x_2 {setup.t_0_ + new_y_and_err.get_c_i(2) * setup.h_};
+  EXPECT_EQ(x_2, 0.25);
+
+  new_y_and_err.sum_a_and_k_products<std::vector<double>, 1>(
+    k_coefficients,
+    2,
+    setup.h_,
+    setup.y_out_);
+
+  std::transform(
+    setup.y_out_.begin(),
+    setup.y_out_.end(),
+    setup.y_0_.begin(),
+    setup.y_out_.begin(),
+    std::plus<double>());
+
+  EXPECT_EQ(setup.y_out_.at(0), 0.875);
+
+  f(x_2, setup.y_out_, k_coefficients.ith_coefficient(2));
+
+  EXPECT_EQ(k_coefficients.get_ith_coefficient(2).at(0), 1.8125);
+
+  // l = 3
+
+  const double x_3 {setup.t_0_ + new_y_and_err.get_c_i(3) * setup.h_};
+
+  new_y_and_err.sum_a_and_k_products<std::vector<double>, 1>(
+    k_coefficients,
+    3,
+    setup.h_,
+    setup.y_out_);
+
+  std::transform(
+    setup.y_out_.begin(),
+    setup.y_out_.end(),
+    setup.y_0_.begin(),
+    setup.y_out_.begin(),
+    std::plus<double>());
+
+  EXPECT_EQ(setup.y_out_.at(0), 0.953125);
+
+  f(x_3, setup.y_out_, k_coefficients.ith_coefficient(3));
+
+  EXPECT_EQ(k_coefficients.get_ith_coefficient(3).at(0), 1.890625);
+
+  // l = 3
+
+  const double x_4 {setup.t_0_ + new_y_and_err.get_c_i(4) * setup.h_};
+
+  new_y_and_err.sum_a_and_k_products<std::vector<double>, 1>(
+    k_coefficients,
+    4,
+    setup.h_,
+    setup.y_out_);
+
+  std::transform(
+    setup.y_out_.begin(),
+    setup.y_out_.end(),
+    setup.y_0_.begin(),
+    setup.y_out_.begin(),
+    std::plus<double>());
+
+  EXPECT_EQ(setup.y_out_.at(0), 1.4453125);
+
+  f(x_4, setup.y_out_, k_coefficients.ith_coefficient(4));
+
+  EXPECT_EQ(k_coefficients.get_ith_coefficient(4).at(0), 2.1953125);
+}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+TEST(TestCalculateNewYAndError, CalculateNewYCalculatesKCoefficientsForRK4)
+{
+  ExampleSetup setup {};
+  KCoefficients<RK4_s, vector<double>> k_coefficients;
+  Examplef f {};
+
+  CalculateNewYAndError<RK4_s, Examplef> new_y_and_err {
+    f,
+    RK4_a_coefficients,
+    RK4_c_coefficients,
+    RK4_b_coefficients};
+
+  // Step 1
+
+  new_y_and_err.calculate_new_y<vector<double>, 1>(
+    setup.h_,
+    setup.t_0_,
+    setup.y_0_,
+    setup.dydx_0_,
+    k_coefficients,
+    setup.y_out_);
+
+  EXPECT_DOUBLE_EQ(k_coefficients.get_ith_coefficient(1).at(0), 1.5);
+  EXPECT_DOUBLE_EQ(k_coefficients.get_ith_coefficient(2).at(0), 1.8125);
+  EXPECT_DOUBLE_EQ(k_coefficients.get_ith_coefficient(3).at(0), 1.890625);
+  EXPECT_DOUBLE_EQ(k_coefficients.get_ith_coefficient(4).at(0), 2.1953125);
+
+  EXPECT_DOUBLE_EQ(setup.y_out_.at(0), 1.4453125);
+
+  setup.y_out_ = setup.y_0_;
+
+  for (std::size_t i {1}; i <= RK4_s; ++i)
+  {
+    setup.y_out_[0] += setup.h_ * (
+      RK4_b_coefficients.get_ith_element(i) *
+        k_coefficients.get_ith_coefficient(i).at(0));
+  }
+
+  EXPECT_DOUBLE_EQ(setup.y_out_[0], 1.4251302083333333);
+
+  // Step 2
+
+  vector<double> dydx {f(setup.t_0_ + setup.h_, setup.y_out_)};
+
+  EXPECT_EQ(dydx.size(), 1);
+  EXPECT_DOUBLE_EQ(dydx.at(0), 2.175130208333333);
+
+  vector<double> y_in {setup.y_out_};  
+  new_y_and_err.calculate_new_y<vector<double>, 1>(
+    setup.h_,
+    setup.t_0_ + setup.h_,
+    y_in,
+    dydx,
+    k_coefficients,
+    setup.y_out_);
+
+  EXPECT_EQ(k_coefficients.get_ith_coefficient(1).size(), 1);
+  EXPECT_DOUBLE_EQ(
+    k_coefficients.get_ith_coefficient(1).at(0),
+    2.175130208333334);
+  EXPECT_EQ(k_coefficients.get_ith_coefficient(2).size(), 1);
+  EXPECT_DOUBLE_EQ(
+    k_coefficients.get_ith_coefficient(2).at(0),
+    2.406412760416666);
+  EXPECT_DOUBLE_EQ(
+    k_coefficients.get_ith_coefficient(3).at(0),
+    2.4642333984375);
+  EXPECT_DOUBLE_EQ(
+    k_coefficients.get_ith_coefficient(4).at(0),
+    2.657246907552084);
+
+  EXPECT_DOUBLE_EQ(setup.y_out_.at(0), 2.657246907552083);
+  setup.y_out_ = y_in;
+
+  for (std::size_t i {1}; i <= RK4_s; ++i)
+  {
+    setup.y_out_[0] += setup.h_ * (
+      RK4_b_coefficients.get_ith_element(i) *
+        k_coefficients.get_ith_coefficient(i).at(0));
+  }
+
+  EXPECT_DOUBLE_EQ(setup.y_out_[0], 2.639602661132812);
+
+  // Step 3
+
+  dydx = f(setup.t_0_ + 2 * setup.h_, setup.y_out_);
+
+  EXPECT_EQ(dydx.size(), 1);
+  EXPECT_DOUBLE_EQ(dydx.at(0), 2.6396026611328125);
+
+  y_in = setup.y_out_;
+  new_y_and_err.calculate_new_y<vector<double>, 1>(
+    setup.h_,
+    setup.t_0_ + 2 * setup.h_,
+    y_in,
+    dydx,
+    k_coefficients,
+    setup.y_out_);
+
+  EXPECT_EQ(k_coefficients.get_ith_coefficient(1).size(), 1);
+  EXPECT_DOUBLE_EQ(
+    k_coefficients.get_ith_coefficient(1).at(0),
+    2.639602661132812);
+  EXPECT_EQ(k_coefficients.get_ith_coefficient(2).size(), 1);
+  EXPECT_DOUBLE_EQ(
+    k_coefficients.get_ith_coefficient(2).at(0),
+    2.737003326416016);
+  EXPECT_DOUBLE_EQ(
+    k_coefficients.get_ith_coefficient(3).at(0),
+    2.761353492736816);
+  EXPECT_DOUBLE_EQ(
+    k_coefficients.get_ith_coefficient(4).at(0),
+    2.77027940750122);
+
+  EXPECT_DOUBLE_EQ(setup.y_out_.at(0), 4.0202794075012207);
+  setup.y_out_ = y_in;
+
+  for (std::size_t i {1}; i <= RK4_s; ++i)
+  {
+    setup.y_out_[0] += setup.h_ * (
+      RK4_b_coefficients.get_ith_element(i) *
+        k_coefficients.get_ith_coefficient(i).at(0));
+  }
+
+  EXPECT_DOUBLE_EQ(setup.y_out_[0], 4.006818970044454);
+}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+TEST(TestCalculateNewYAndError,
+  CalculateNewYCalculatesKCoefficientsForDormandPrince5Coefficients)
+{
+  ExampleSetup setup {};
+  Examplef f {};
 
   CalculateNewYAndError new_y_and_err {
-    Examplef{},
+    f,
     DOPRI5_a_coefficients,
     DOPRI5_c_coefficients,
     DOPRI5_delta_coefficients};
+
+  // Step 1.
 
   new_y_and_err.calculate_new_y<vector<double>, 1>(
     setup.h_,
@@ -427,7 +660,89 @@ TEST(TestCalculateNewYAndError, CalculateNewYCalculatesKCoefficients)
   EXPECT_DOUBLE_EQ(setup.k_coefficients_.get_ith_coefficient(7).at(0),
     2.175644097222222);
 
-  EXPECT_DOUBLE_EQ(setup.y_out_.at(0), 1.425644097222222);
+  EXPECT_NEAR(setup.y_out_.at(0), exact_solution(setup.h_), 1e-5);
+
+  // Step 2.
+
+  vector<double> dydx {f(setup.t_0_ + setup.h_, setup.y_out_)};
+
+  EXPECT_EQ(dydx.size(), 1);
+  EXPECT_DOUBLE_EQ(dydx.at(0), 2.175644097222222);
+
+  vector<double> y_in {setup.y_out_};  
+
+  new_y_and_err.calculate_new_y<vector<double>, 1>(
+    setup.h_,
+    setup.t_0_ + setup.h_,
+    y_in,
+    dydx,
+    setup.k_coefficients_,
+    setup.y_out_);
+
+  EXPECT_DOUBLE_EQ(
+    setup.k_coefficients_.get_ith_coefficient(1).at(0),
+    2.175644097222222);
+  EXPECT_DOUBLE_EQ(
+    setup.k_coefficients_.get_ith_coefficient(2).at(0),
+    2.2832085069444443);
+  EXPECT_DOUBLE_EQ(
+    setup.k_coefficients_.get_ith_coefficient(3).at(0),
+    2.341591707899305);
+  EXPECT_DOUBLE_EQ(
+    setup.k_coefficients_.get_ith_coefficient(4).at(0),
+    2.5801328124999987);
+  EXPECT_DOUBLE_EQ(
+    setup.k_coefficients_.get_ith_coefficient(5).at(0),
+    2.633202642222983);
+  EXPECT_DOUBLE_EQ(
+    setup.k_coefficients_.get_ith_coefficient(6).at(0),
+    2.667628162582859);
+  EXPECT_DOUBLE_EQ(
+    setup.k_coefficients_.get_ith_coefficient(7).at(0),
+    2.6408707492856616);
+
+  EXPECT_NEAR(setup.y_out_.at(0), exact_solution(2 * setup.h_), 1e-4);
+
+  // Step 3.
+
+  dydx = f(setup.t_0_ + 2 * setup.h_, setup.y_out_);
+
+  EXPECT_EQ(dydx.size(), 1);
+  EXPECT_DOUBLE_EQ(dydx.at(0), 2.6408707492856616);
+
+  y_in = setup.y_out_;
+
+  new_y_and_err.calculate_new_y<vector<double>, 1>(
+    setup.h_,
+    setup.t_0_ + 2 * setup.h_,
+    y_in,
+    dydx,
+    setup.k_coefficients_,
+    setup.y_out_);
+
+  EXPECT_DOUBLE_EQ(
+    setup.k_coefficients_.get_ith_coefficient(1).at(0),
+    2.6408707492856616);
+  EXPECT_DOUBLE_EQ(
+    setup.k_coefficients_.get_ith_coefficient(2).at(0),
+    2.694957824214227);
+  EXPECT_DOUBLE_EQ(
+    setup.k_coefficients_.get_ith_coefficient(3).at(0),
+    2.7205861576079746);
+  EXPECT_DOUBLE_EQ(
+    setup.k_coefficients_.get_ith_coefficient(4).at(0),
+    2.77797279059516);
+  EXPECT_DOUBLE_EQ(
+    setup.k_coefficients_.get_ith_coefficient(5).at(0),
+    2.786162739074309);
+  EXPECT_DOUBLE_EQ(
+    setup.k_coefficients_.get_ith_coefficient(6).at(0),
+    2.7745870563781194);
+  EXPECT_DOUBLE_EQ(
+    setup.k_coefficients_.get_ith_coefficient(7).at(0),
+    2.7591771182645264);
+
+  EXPECT_NEAR(setup.y_out_.at(0), exact_solution(3 * setup.h_), 1e-4);
 }
 
 //------------------------------------------------------------------------------
@@ -464,11 +779,11 @@ TEST(TestCalculateNewYAndError, CalculatesNewYAndError)
     setup.y_out_,
     setup.y_err_);
 
-  EXPECT_DOUBLE_EQ(setup.y_out_.at(0), 2.3820792665834776);
-  EXPECT_DOUBLE_EQ(setup.y_err_.at(0), 0.00039204193491992542);
-  EXPECT_NEAR(setup.y_out_.at(0), exact_solution(setup.h_ + setup.h_), 1e-5);
+  EXPECT_DOUBLE_EQ(setup.y_out_.at(0), 2.6408707492856616);
+  EXPECT_DOUBLE_EQ(setup.y_err_.at(0), -1.7718829684792992e-05);
+  EXPECT_NEAR(setup.y_out_.at(0), exact_solution(setup.h_ + setup.h_), 1e-4);
 }
-*/
+
 } // namespace RKMethods
 } // namespace ODE 
 } // namespace Numerical
