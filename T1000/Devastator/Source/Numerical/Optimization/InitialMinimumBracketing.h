548769b534f5956384d9c1625a93e6d22a21b64d
Ernest Yeung <ernestyalumni@gmail.com>
2023-04-28 05:43:23 -0700

    Added Initial Minimum Bracketing

---
+++ T1000/Devastator/Source/Numerical/Optimization/InitialMinimumBracketing.h
@@ -0,0 +1,26 @@
+#ifndef NUMERICAL_OPTIMIZATION_INITIAL_MINIMUM_BRACKETING_H
+#define NUMERICAL_OPTIMIZATION_INITIAL_MINIMUM_BRACKETING_H
+
+#include "FindParabolicInterpolation.h"
+
+#include <math.h> // std::copysign
+#include <optional>
+#include <utility> // std::swap
+
+using std::swap;
+
+namespace Numerical
+{
+namespace Optimization
+{
+
+struct InitialMinimumBracketing
+{
+
+};
+
+} // namespace Optimization
+} // namespace Numerical
+
+#endif // NUMERICAL_OPTIMIZATION_INITIAL_MINIMUM_BRACKETING_H
+
