3b4021dba4a89edfd1b8d16fadfcb682eae79bf4
Ernest Yeung <ernestyalumni@gmail.com>
2026-03-07 17:34:29 -0800

    Remove Flask related tutorial code; there are better frameworks now

--- T1000/PacktExample/manage.py
+++
@@ -1,7 +0,0 @@
-from . import configurations
-from .WebApp.create_application import create_application
-
-app = create_application(configurations.DevelopmentConfiguration())
-
-@app.shell_context_processor
-def make_shell_context():
--- T1000/T1000/UdemyFlaskRestful/section3/app.py
+++
@@ -1,8 +0,0 @@
-"""
-@file app.py
-
-@details The name app.py is special to Flask; it's immediately recognized to be run.
-
-"""
-
-from flask import Flask, jsonify, request
